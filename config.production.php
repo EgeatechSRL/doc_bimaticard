3f58d89b607c63dd1c1958abad36ee7bc990a379
marco76tv <marco.sottana@gmail.com>
2023-10-19 11:48:02 +0200

    .

--- config.production.php
+++ config.production.php
@@ -1,7 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-    'baseUrl' => 'https://jigsaw-docs-template.tighten.co',
+    'baseUrl' => 'https://egeatechsrl.github.io/doc_bimaticard',
     'production' => true,
 
     // DocSearch credentials
